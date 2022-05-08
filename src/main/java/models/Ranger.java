a768facbb38015a5eba3a71bfd6254ad99517bd4
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-08 15:58:58 +0300

    File updates Ranger class getters and setters created

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -1,4 +1,48 @@
 package models;
 
+import jdk.jfr.Timestamp;
+
 public class Ranger {
+    private String rangerName;
+    private int rangerId;
+    private int radioFrequency;
+    private String avatarUrl;
+
+    public Ranger(String rangerName, int radioFrequency, String avatarUrl) {
+        this.rangerName = rangerName;
+        this.radioFrequency = radioFrequency;
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    public int getRangerId() {
+        return rangerId;
+    }
+
+    public int getRadioFrequency() {
+        return radioFrequency;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setRangerName(String rangerName) {
+        this.rangerName = rangerName;
+    }
+
+    public void setRangerId(int rangerId) {
+        this.rangerId = rangerId;
+    }
+
+    public void setRadioFrequency(int radioFrequency) {
+        this.radioFrequency = radioFrequency;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
 }
