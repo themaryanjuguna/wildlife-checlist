6b7123fcb82bf5e8f431f7253bc9bda8ece20eb7
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-11 14:02:14 +0300

    App Java file Database connection added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import dao.Sql2oAnimalsDao;
+import dao.Sql2oLocationDao;
+import dao.Sql2oRangerDao;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -6,10 +10,18 @@ import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
+import static spark.route.HttpMethod.get;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        String connectionString = "jdbc:postgresql://localhost:5432/wildlife_tracker";
+        Sql2o sql2o = new Sql2o(connectionString, "maryanne", "myPassword");
+        Sql2oRangerDao RangerDao  = new Sql2oRangerDao(sql2o);
+        Sql2oLocationDao LocationDao  = new Sql2oLocationDao(sql2o);
+        Sql2oAnimalsDao AnimalDao  = new Sql2oAnimalsDao(sql2o);
+
 
         get("/", (request, response) ->{
             Map<String, Object> model = new HashMap<String, Object>();
