5e751389373e69c3cb202ad3de82e215ce889608
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-11 17:21:53 +0300

    App Java update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,15 @@
 import dao.Sql2oAnimalsDao;
 import dao.Sql2oLocationDao;
 import dao.Sql2oRangerDao;
+import models.Animals;
+import models.Location;
+import models.Ranger;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -22,10 +26,25 @@ public class App {
         Sql2oLocationDao LocationDao  = new Sql2oLocationDao(sql2o);
         Sql2oAnimalsDao AnimalDao  = new Sql2oAnimalsDao(sql2o);
 
-
+        //Show all rangers, animals and location
         get("/", (request, response) ->{
             Map<String, Object> model = new HashMap<String, Object>();
+            List<Animals>allanimals = AnimalDao.getAllAnimals();
+            model.put("animals", allanimals);
+            List<Ranger>allrangers = RangerDao.getallRangers();
+            model.put("ranger", allrangers);
+            List<Location>alllocation = LocationDao.getAllLocations();
+            model.put("location", alllocation);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //Display form to add content
+        get("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Animals> animals = AnimalDao.getAllAnimals();
+            model.put("animals", animals);
+            return  new ModelAndView(model, "new-animal.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
